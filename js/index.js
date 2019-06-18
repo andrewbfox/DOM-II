e488a6d5152089e3dc17a17a1146a7cf32bfbf7a
Andrew Fox <andrewbfox@gmail.com>
2019-06-18 16:02:03 -0400

    Modified readme.md to check boxes; modified index.js to add mouseover and mouseleave event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+// Change h1 color
+const headerH1 = document.querySelector("h1");
+headerH1.addEventListener("mouseover", (event) => {
+    headerH1.style.color = "red";
+});
+headerH1.addEventListener("mouseleave", (event) => {
+    headerH1.style.color = "green";
+});
