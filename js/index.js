f651d2790949e115c97e503d0800bc3573d1e072
Andrew Fox <andrewbfox@gmail.com>
2019-06-18 16:42:03 -0400

    Modified index.js to change background on copy; modified readme.md to check boxes

--- js/index.js
+++ js/index.js
@@ -10,8 +10,13 @@ headerH1.addEventListener("mouseleave", (event) => {
 });
 
 // Hide image
-
 const headerH2 = document.querySelector("div header img");
 headerH2.addEventListener("dblclick", (event) => {
         headerH2.style.visibility = "hidden";
 });
+
+// Change background on copy
+const para = document.querySelector("p");
+para.addEventListener('copy', (event) => {
+    event.target.style.background = "green";
+})
